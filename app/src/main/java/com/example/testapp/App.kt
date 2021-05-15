a5b1f3ea31d669b9a5f3312d3755de2e3199c20e
KuzichkinDA <dlpmnzzz@gmail.com>
2021-05-15 12:37:30 +0300

    fix dependency and add base classes

--- app/src/main/java/com/example/testapp/App.kt
+++ app/src/main/java/com/example/testapp/App.kt
@@ -2,7 +2,6 @@ package com.example.testapp
 
 import android.app.Application
 import com.example.testapp.di.appModule
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
@@ -10,7 +9,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidContext(this@App)
             modules(listOf(appModule))
         }
     }
